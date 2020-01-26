d9232d460bc040b49f9af8fb75099c8de225726c
Corodescu Mihnea <corodescu.mihnea@gmail.com>
2020-01-26 20:22:20 +0200

    Created pages,styles,scripts folders and a simple login and register page

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
-
-
-
+// Include 
 var fs = require('fs');
-var http = require('http');
 const path = require('path');
 var https = require('https');
+// Set cert 
 var privateKey  = fs.readFileSync('server.key', 'utf8');
 var certificate = fs.readFileSync('server.cert', 'utf8');
-
 var credentials = {key: privateKey, cert: certificate};
+
 var express = require('express');
 var app = express();
-app.use(express.static('C:\\Users\\Alex\\Desktop\\dawjs proiect\\Picolo'));
 
-
-// your express configuration here
+// TODO: add route dispath
 app.get('/',function(req,res){
-   res.sendFile(path.join(__dirname+'/index.html'));
+   res.sendFile(path.join(__dirname+'/pages/login.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(3000);
+httpsServer.listen(8008);
